fe0bebe1323e9d9b177d0efbbaf9a6a688306b8b
Viktor Zerkin <v.zerkin@gmail.com>
2023-11-09 13:59:07 +0100

    split00-split.sh:2023-11-09 git:2023-11-09,14:34

---
+++ test/split00-split.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+split -d --verbose -b 100m EXFOR-2023-04-29.zip EXFOR-2023-04-29.zip_
+split -d --verbose -b 100m EXFOR-2023-05-15.zip EXFOR-2023-05-15.zip_
+split -d --verbose -b 100m EXFOR-2023-05-23.zip EXFOR-2023-05-23.zip_

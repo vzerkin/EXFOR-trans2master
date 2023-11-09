994a5be60aafb75d767711b0be40ea5c010b50b7
Viktor Zerkin <v.zerkin@gmail.com>
2023-11-09 14:00:06 +0100

    step0-unzip.bat:2023-11-09 git:2023-11-09,14:34

---
+++ test/step0-unzip.bat
@@ -0,0 +1,12 @@
+
+type EXFOR-2023-04-29.zip_* >EXFOR-2023-04-29.zip
+type EXFOR-2023-05-15.zip_* >EXFOR-2023-05-15.zip
+type EXFOR-2023-05-23.zip_* >EXFOR-2023-05-23.zip
+
+dir EXFOR-2023-04-29.zip
+dir EXFOR-2023-05-15.zip
+dir EXFOR-2023-05-23.zip
+
+unzip EXFOR-2023-04-29.zip EXFOR-2023-04-29.bck
+unzip EXFOR-2023-05-15.zip EXFOR-2023-05-15.bck
+unzip EXFOR-2023-05-23.zip EXFOR-2023-05-23.bck

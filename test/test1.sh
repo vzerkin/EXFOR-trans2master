7d3ab5049b58f81f597aa060cf1ebfabd57a20c6
Viktor Zerkin <v.zerkin@gmail.com>
2023-11-09 14:33:52 +0100

    test1.sh:2023-11-09 git:2023-11-09,14:34

--- test/test1.sh
+++ test/test1.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
-#To run this test, you must have in the current directory:
-#	EXFOR-2023-04-29.bck
-#	trans.c222
-#	EXFOR-2023-05-15.bck
-echo "Script [$0] start `date +%F,%T` `uname -n`/`uname -s`"
+user="$USER"
+if [ "$user" = "" ] ; then user="${USERNAME}" ; fi
+if [ "$user" = "" ] ; then user="${LOGNAME}"  ; fi
+if [ "$user" = "" ] ; then user="`id -u -n`"  ; fi
+
+echo "Script [$0] start `date +%F,%T` on:`uname -n`/`uname -s` user:$user"
 java -version
+
+chkFile() {
+    if [ -f $1 ]; then 
+	echo "---checking file existance [$1]: OK."
+    else
+	echo -e "\n___ERROR: file not found: [$1]"
+	exit
+    fi
+}
+chkFiles() {
+    for file1 in $@; do
+	chkFile $file1
+    done
+}
+
+#To run this test you must have files in the current directory:
+chkFiles EXFOR-2023-04-29.bck trans.c222 EXFOR-2023-05-15.bck
+
 t00=`date +%s`
 set -x
 
-#java -cp x4master.jar trans2master \
-java -Xmx4000M -cp x4master.jar trans2master \
+#java -Xmx4000M -cp x4master.jar trans2master \
+java -cp x4master.jar trans2master \
  -i:EXFOR-2023-04-29.bck \
  -n3set:20230515 -t:trans.c222 \
  -h:REQUEST,1001,20230515,172831,20230515,3 \
  -o:EXFOR-2023-05-15.new
 errCode=$?
+set +x
 if [ $errCode -ne 0 ]; then 
-  set +x
   echo "------Error=$errCode";
   echo "Please, check your java version.";
   java -version

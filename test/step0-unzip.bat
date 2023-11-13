dc8c5f2d99810d7a7f345f12f72bddbb0b65a397
Viktor Zerkin <v.zerkin@gmail.com>
2023-11-13 10:15:34 +0100

    step0-unzip.bat:2023-11-13 git:2023-11-13,10:32

--- test/step0-unzip.bat
+++ test/step0-unzip.bat
@@ -10,3 +10,5 @@ dir EXFOR-2023-05-23.zip
 unzip EXFOR-2023-04-29.zip EXFOR-2023-04-29.bck
 unzip EXFOR-2023-05-15.zip EXFOR-2023-05-15.bck
 unzip EXFOR-2023-05-23.zip EXFOR-2023-05-23.bck
+
+@rem "C:\Program Files\7-Zip\7z.exe" x EXFOR-2023-05-23.zip EXFOR-2023-05-23.bck
